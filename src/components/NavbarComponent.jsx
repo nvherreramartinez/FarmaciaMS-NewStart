a63eade66f71ba6df6b196a6e7c44ee89428fa6b
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2026-04-21 13:58:29 -0300

    Update NavbarComponent link to point to categories

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -6,7 +6,7 @@ const NavbarComponent = () =>{
     return(
         <nav className="navContainer">
             <NavLink className="aLink" to='/'>Farmacia MS</NavLink>
-            <NavLink className="aLink" to='/producto/laboratorios'>Laboratorios</NavLink>
+            <NavLink className="aLink" to='/category/laboratorios'>Laboratorios</NavLink>
             <NavLink className="aLink" to='/producto/Ofertas'>Ofertas</NavLink>
             <NavLink className="aLink" to='/producto/obra sociales'>Obras Sociales</NavLink>
             <NavLink className='aLink' to='/cart'><CartWidget counter={15}/></NavLink>
