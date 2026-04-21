bd2e9c6f16b869ff1d15136c889866063d8e79c3
Natalia Vanesa Herrera <nvherreramartinez@gmail.com>
2026-04-21 15:20:15 -0300

    Update CartContainer layout and add cart footer
    Refactor CartContainer to improve layout and styling.

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -7,17 +7,19 @@ import { Link } from "react-router-dom"
 const CartContainer = () => {
     const{cart, cartTotal, clear} = useContext(CartContext)
     return (
-        <div>
-        {!cart.length ? <EmpyCart/>
-        : <div className="cartWidgetContainer">
-                <h2>Tu carrito</h2>
-                {cart.map((producto) => <CartItem key={producto.id} producto={producto}/>)}
-                <span>Monto total de su compra: ${cartTotal()}</span>
-                <div>
-                    <button className="btn btn-danger" onClick={clear}>Borrar carrito</button>
-                    <Link className='btn btn-success' to='/checkout'>Finalizar Compra</Link>
+        <div className="cart-list-container"> 
+            {!cart.length ? <EmpyCart />
+                : <div>
+                    <h2>Tu carrito</h2>
+                    {cart.map((producto) => <CartItem key={producto.id} producto={producto} />)}
+                    <div className="cart-footer"> 
+                        <span className="total-amount">Monto total de su compra: ${cartTotal()},00</span>
+                        <div className="cart-buttons">
+                            <button className="btn btn-danger" onClick={clear}>Borrar carrito</button>
+                            <Link className='btn btn-success' to='/checkout'>Finalizar Compra</Link>
+                        </div>
+                    </div>
                 </div>
-            </div>
             }
         </div>
     )
